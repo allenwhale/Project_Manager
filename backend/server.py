b433535d1f6a2ac4ba0c7dde8acffb60b709c42c
allenwhale <allenwhale@yahoo.com.tw>
2015-03-06 15:11:48 +0800

    update tornado multi processes

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,8 @@
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
+import tornado.process
+import tornado.netutil
 from req import Service
 from req import reqenv
 from req import RequestHandler
@@ -14,11 +16,12 @@ class IndexHandler(RequestHandler):
         return
 
 if __name__ == '__main__':
+    httpsock = tornado.netutil.bind_sockets(config.PORT)
+    tornado.process.fork_processes(config.PROCESSES)
     app = tornado.web.Application([
         ('/',IndexHandler),
         ('/(.*)', tornado.web.StaticFileHandler, {'path': config.STATIC_FILE_DIR}),
         ], cookie_secret=config.COOKIE_SECRET, autoescape='xhtml_escape')
     srv = tornado.httpserver.HTTPServer(app)
-    srv.bind(config.PORT)
-    srv.start(config.THREADS)
+    srv.add_sockets(httpsock)
     tornado.ioloop.IOLoop.instance().start()
